6631bbae8306cbdda4123fb5d3f07c976d3c28cf
B Blue <b@b-blue.dev>
2025-06-08 12:02:08 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Question from './Question'
+
+function App() {
+  return (
+    <>
+      <Question/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Question.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import questions from "../questions.json"; // Assuming you have a JSON file with questions
+
+const Question: React.FC = () => {
+  const [index, setIndex] = useState<number>(() =>
+    Math.floor(Math.random() * questions.length)
+  );
+
+  const nextQuestion = () => {
+    let newIndex = Math.floor(Math.random() * questions.length);
+    while (newIndex === index && questions.length > 1) {
+      newIndex = Math.floor(Math.random() * questions.length);
+    }
+    setIndex(newIndex);
+  };
+
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <button
+        onClick={nextQuestion}
+        style={{
+          marginTop: "20px",
+          padding: "24px 32px",
+          fontSize: "24px",
+          fontWeight: "bold",
+          cursor: "pointer",
+          border: "2px solid black",
+          borderRadius: "8px",
+          display: "inline-flex",
+          justifyContent: "center",
+          alignItems: "center",
+          whiteSpace: "pre-line",
+        }}
+      >
+        {questions[index]}
+      </button>
+    </div>
+  );
+};
+
+export default Question;
